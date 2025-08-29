d82ed360556ebd5f632e1fd6d1807b8620a9a033
Taylor Barnes <taylor.a.barnes@gmail.com>
2025-08-29 14:00:58 -0400

    Access wrapper through dlopen

--- librepo/include/mpiwrapper.h
+++ librepo/include/mpiwrapper.h
@@ -5,9 +5,9 @@
 #include<mpi.h>
 #endif
 
-int mp_init( int *argc, char ***argv );
-int mp_finalize(void);
-int mp_comm_rank( int *rank );
-int mp_comm_size( int *size );
+extern "C" int mp_init( int *argc, char ***argv );
+extern "C" int mp_finalize(void);
+extern "C" int mp_comm_rank( int *rank );
+extern "C" int mp_comm_size( int *size );
 
 #endif
--- repo/src/main.cpp
+++ repo/src/main.cpp
@@ -1,19 +1,58 @@
-#include "mpiwrapper.h"
 #include <iostream>
+#include <dlfcn.h>
 
 int main(int argc, char** argv) {
-    mp_init(&argc, &argv);
+  void* lib_handle = dlopen("/work/librepo/build/libMPIWrapper.so", RTLD_LAZY);
+  if ( !lib_handle ) {
+    std::cerr << "dlopen failed: " << dlerror() << '\n';
+    return 1;
+  }
 
-    int world_size;
-    mp_comm_size(&world_size);
+  typedef int(*mp_init_t)(int*, char***);
+  mp_init_t mp_init;
+  mp_init = (mp_init_t) (intptr_t) dlsym(lib_handle, "mp_init");
+  if ( !mp_init ) {
+    std::cerr << "dlsym failed: " << dlerror() << '\n';
+    return 2;
+  }
 
-    int world_rank;
-    mp_comm_rank(&world_rank);
+  typedef int(*mp_finalize_t)();
+  mp_finalize_t mp_finalize;
+  mp_finalize = (mp_finalize_t) (intptr_t) dlsym(lib_handle, "mp_finalize");
+  if ( !mp_finalize ) {
+    std::cerr << "dlsym failed: " << dlerror() << '\n';
+    return 2;
+  }
 
-    if (world_rank == 0) {
-        std::cout << "World size: " << world_size << std::endl;
-    }
+  typedef int(*mp_comm_rank_t)(int*);
+  mp_comm_rank_t mp_comm_rank;
+  mp_comm_rank = (mp_comm_rank_t) (intptr_t) dlsym(lib_handle, "mp_comm_rank");
+  if ( !mp_comm_rank ) {
+    std::cerr << "dlsym failed: " << dlerror() << '\n';
+    return 2;
+  }
 
-    mp_finalize();
-    return 0;
+  typedef int(*mp_comm_size_t)(int*);
+  mp_comm_size_t mp_comm_size;
+  mp_comm_size = (mp_comm_size_t) (intptr_t) dlsym(lib_handle, "mp_comm_size");
+  if ( !mp_comm_size ) {
+    std::cerr << "dlsym failed: " << dlerror() << '\n';
+    return 2;
+  }
+
+  mp_init(&argc, &argv);
+
+  int world_size;
+  mp_comm_size(&world_size);
+
+  int world_rank;
+  mp_comm_rank(&world_rank);
+
+  if (world_rank == 0) {
+    std::cout << "World size: " << world_size << std::endl;
+  }
+
+  mp_finalize();
+
+  return 0;
 }
